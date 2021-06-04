07e0bf593a7d482c4ba185595d7a93bc8360cd9b
Devashish Patil <devashishpatil.patil@gmail.com>
2021-06-04 23:26:33 +0530

    change

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var axios = require('axios');
+var request = require('request');
 var cheerio = require('cheerio');
 
 
@@ -11,13 +11,45 @@ app.use(express.urlencoded({
 
 app.get('/', (req ,res) => {
     try {
-        res.send("Hello")
+
+        request('https://www.moneycontrol.com/stocks/marketinfo/bonus/index.php?sel_year=2021', (error, response, html) => {
+                if (!error && response.statusCode == 200) {
+                    var $ = cheerio.load(html)
+
+                    var keys = [
+                        'Company',
+                        'Bonus_Ratio',
+                        'Announcement_Date',
+                        'Record_Date',
+                        'ExBonus_Date'
+                    ]
+                    
+                    var tableArr = []
+                    
+                    $(".dvdtbl tbody tr").each((i, el) => {
+                        var tableData = {}
+                        var link_placements = $(el).find('a').attr('href');
+                        if (i>1){
+                            $(el).children().each((ci, cel) => {
+                                var tdValues = $(cel).text()
+                                if (tdValues){
+                                    tableData[keys[ci]] = tdValues
+                                    tableData["Link"] = "https://www.moneycontrol.com" + link_placements
+                                }
+                            })
+                            tableArr.push(tableData)
+                        }
+                    })
+
+                    res.send(tableArr)
+                }
+            });
 
     } catch (error) {
         console.error(error)
     }
 });
 
-app.listen(process.env.PORT || 8080, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("App running")
 });
